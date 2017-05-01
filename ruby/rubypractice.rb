1185aae7b564e2abbd107613ced44d2417a706f6
Kevin Cross <Kevinhcross96@gmail.com>
2017-05-01 00:38:28 -0400

    completed module 5.7

--- ruby/rubypractice.rb
+++ ruby/rubypractice.rb
@@ -11,3 +11,5 @@ end
 puts "After .each call:"
 p letters
 p new_letters
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
